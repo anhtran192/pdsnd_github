ca486be0a2656bab9bfcbf9d1914fa4d6cd1f186
anhtran192 <anhtranbc23@gmail.com>
2023-10-19 17:23:02 +0700

    type: add description msg

--- bikeshare.py
+++ bikeshare.py
@@ -2,10 +2,11 @@ import time
 import pandas as pd
 import numpy as np
 
+#add
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
-
+#function
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
