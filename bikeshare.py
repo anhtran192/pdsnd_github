897779dd15289b30b767ebe59fc15787d8c95e27
anhtran192 <anhtranbc23@gmail.com>
2023-10-19 17:23:55 +0700

    type: add description msg3

--- bikeshare.py
+++ bikeshare.py
@@ -6,7 +6,7 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
-#function
+#function started
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
